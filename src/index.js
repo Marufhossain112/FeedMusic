df35bf372dc503161233f257ee22262a73d87f93
Maruf Hossain <dev.maruf112@gmail.com>
2024-01-04 14:03:53 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Home';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
